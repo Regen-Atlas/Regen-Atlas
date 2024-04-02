fd9bd181ee9a34cccea7fc23ad43c92a2f6a889f
Louise <louise.borreani@gmail.com>
2024-04-02 13:25:37 +0200

    Add server.js for Heroku deployment

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files from the root directory
+app.use(express.static(__dirname));
+
+// Serve the CSS as a static resource
+app.use('/css', express.static(path.join(__dirname, 'css')));
+
+// Respond to all GET requests by rendering the index.html file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
